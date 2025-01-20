e4fa628207ac85eb0f026649e9742cbfc2899d06
lunatics4 <l4p@awc.tec.br>
2025-01-20 14:18:17 -0300

    infra

---
+++ infra/Config.go
@@ -0,0 +1,91 @@
+package infra
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v7"
+	"github.com/jmscatena/Fatec_Sert_SGCourse/dto/migrations"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+type Connection struct {
+	Db    *gorm.DB
+	NoSql *redis.Client
+}
+
+func (c *Connection) InitDB() (*gorm.DB, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error Loading Configuration File")
+	}
+
+	dbUser := os.Getenv("DBUSER")
+	dbPass := os.Getenv("DBPASS")
+	dbase := os.Getenv("DB")
+	dbServer := os.Getenv("DBSERVER")
+	dbPort := os.Getenv("DBPORT")
+	dbURL := "postgres://" + dbUser + ":" + dbPass + "@" + dbServer + ":" + dbPort + "/" + dbase
+
+	c.Db, err = gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+	if err != nil {
+		log.Fatalln("Erro no carregamento do SGBD", err)
+	}
+	migrations.RunMigrate(c.Db)
+	return c.Db, err
+}
+
+func (c *Connection) InitNoSQL() (*redis.Client, error) {
+	dsn := os.Getenv("REDIS_DSN")
+	if len(dsn) == 0 {
+		dsn = "localhost:6379"
+	}
+	c.NoSql = redis.NewClient(&redis.Options{
+		Addr: dsn, //redis port
+	})
+	var _, err = c.NoSql.Ping().Result()
+	if err != nil {
+		log.Fatalln("Erro no carregamento do Redis:", err)
+	}
+	return c.NoSql, nil
+}
+
+type Server struct {
+	Port   string
+	Server *gin.Engine
+}
+
+func (s *Server) NewServer(port string) {
+	s.Port = port
+	s.Server = gin.Default()
+}
+
+func (s *Server) Run() {
+	log.Printf("Server running at port: %v", s.Port)
+
+}
+
+type SecretsToken struct {
+	secret  string
+	refresh string
+}
+
+func (t *SecretsToken) GetAccess() string {
+	return t.secret
+}
+func (t *SecretsToken) GetRefresh() string {
+	return t.refresh
+}
+
+func (t *SecretsToken) GenerateSecret() *SecretsToken {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error Loading Configuration File")
+	}
+	return &SecretsToken{
+		secret:  os.Getenv("TOKEN_SECRET_KEY"),
+		refresh: os.Getenv("REFRESH_SECRET_KEY"),
+	}
+}
---
+++ infra/Token.go
@@ -0,0 +1,79 @@
+package infra
+
+import (
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/jmscatena/Fatec_Sert_SGCourse/dto/models/administrativo"
+	"time"
+)
+
+func CreateToken(user administrativo.Usuario, expire int, secretkey string) (string, error) {
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"uuid": user.UID.String(), // Subject (user identifier)
+		"name": user.Nome,         // Issuer
+		//"exp":  time.Now().Add(time.Hour).Unix(), // Expiration time
+		"exp": time.Now().Add(time.Duration(expire) * time.Minute).Unix(), // Expiration time
+		//"aud":  getRole(username),                // Audience (user role)
+		//"iat":  time.Now().Unix(),                // Issued at
+	})
+	tokenString, err := claims.SignedString([]byte(secretkey))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+func StoreToken(key string, value string, expire int, conn Connection) error {
+	redisClient := conn.NoSql
+	if redisClient == nil {
+		return fmt.Errorf("Error Access Database to store token")
+	}
+	err := redisClient.Set(key, value, time.Minute*time.Duration(expire)).Err()
+	if err != nil {
+		return fmt.Errorf("Error storing token: %w", err)
+	}
+	//defer redisClient.Close()
+	return nil
+}
+
+func VerifyToken(tokenString string, secretKey string) (*jwt.Token, error) {
+	sk := secretKey
+	keyfunc := func(token *jwt.Token) (interface{}, error) {
+		ok := token.Method.Alg() == jwt.SigningMethodHS256.Alg()
+		if !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(sk), nil
+	}
+	claims := jwt.MapClaims{}
+	jwtToken, err := jwt.ParseWithClaims(tokenString, &claims, keyfunc)
+	if err != nil {
+		return nil, err
+	}
+	if !jwtToken.Valid {
+		return nil, fmt.Errorf("Invalid Token")
+	}
+	return jwtToken, nil
+	/*
+		token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return secretKey, nil
+		})
+		println(token.Valid)
+		if !token.Valid {
+			return nil, fmt.Errorf("invalid token")
+		}
+		return token, nil
+	*/
+}
+
+func RevokeToken(token string, conn Connection) error {
+	//redisClient, err := database.InitDF()
+	if conn.NoSql == nil {
+		return fmt.Errorf("Error Database Connection to Data token revoke")
+	}
+	err := conn.NoSql.Del(token).Err()
+	if err != nil {
+		return fmt.Errorf("Error revoke token: %w", err)
+	}
+	//defer redisClient.Close()
+	return nil
+}
