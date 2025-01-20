15d0901eea7569b29aaa4bee933cca6b18e69be1
lunatics4 <l4p@awc.tec.br>
2025-01-20 14:29:36 -0300

    main

---
+++ ccontrol.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jmscatena/Fatec_Sert_SGCourse/infra"
+	"github.com/jmscatena/Fatec_Sert_SGCourse/routes"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error Loading Configuration File")
+	}
+	gin.SetMode(os.Getenv("SET_MODE"))
+	dbConn := infra.Connection{Db: nil, NoSql: nil}
+	_, err = dbConn.InitDB()
+	if err != nil {
+		log.Fatalf("Error Loading Database Connection")
+	}
+	_, err = dbConn.InitNoSQL()
+	if err != nil {
+		log.Fatalf("Error Loading Redis Connection")
+	}
+	token := (&infra.SecretsToken{}).GenerateSecret()
+
+	server := infra.Server{}
+	server.NewServer("8000")
+	router := routes.ConfigRoutes(server.Server, dbConn, *token)
+	log.Fatal(router.Run(":" + server.Port))
+
+}
