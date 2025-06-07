abaa77015c836b96e9a4f8f46b27f523b927a5ba
lunatics4 <l4p@awc.tec.br>
2025-06-07 09:42:24 -0300

    update : Server Proxies

--- config/Config.go
+++ config/Config.go
@@ -8,6 +8,7 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -57,9 +58,42 @@ type Server struct {
 	Server *gin.Engine
 }
 
+func IPMiddleware(allowedIPs []string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Obtém o endereço de IP do cliente.
+		// c.ClientIP() tenta extrair o IP de cabeçalhos como X-Forwarded-For ou X-Real-IP,
+		// o que é útil se a sua API estiver atrás de um proxy reverso ou load balancer.
+		clientIP := c.ClientIP()
+
+		// Verifica se o IP do cliente está na lista de IPs permitidos.
+		allowed := false
+		for _, ip := range allowedIPs {
+			if clientIP == ip {
+				allowed = true
+				break
+			}
+		}
+
+		// Se o IP não for permitido, a requisição é abortada.
+		if !allowed {
+			// c.AbortWithStatusJSON interrompe a cadeia de execução e envia uma resposta JSON.
+			// http.StatusForbidden é o código de status HTTP 403.
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{
+				"status":  "error",
+				"message": "Acesso negado",
+			})
+			return // Encerra a função aqui.
+		}
+
+		// Se o IP for permitido, a requisição continua para o próximo handler.
+		c.Next()
+	}
+}
 func (s *Server) NewServer(port string) {
+	allowedIPs := []string{"127.0.0.1", "200.144.13.38"}
 	s.Port = port
 	s.Server = gin.Default()
+	s.Server.Use(IPMiddleware(allowedIPs))
 }
 
 func (s *Server) Run() {
